dd8b555e6913ad8f92ed6ece52bd56916ac56ea6
= <=>
2019-09-19 13:33:14 +0700

    Add problem infor - Google interview

---
+++ src/main/java/interleav_stack/Solution.java
@@ -0,0 +1,14 @@
+package interleav_stack;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Solution {
+    public Stack<Integer> interleaveStack(Stack<Integer> numbers){
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        return numbers;
+    }
+
+}
